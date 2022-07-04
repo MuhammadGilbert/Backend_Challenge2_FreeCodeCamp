732e36f1b46158ed573e91c0ad1e8c0fa88f8eb9
Muhammad Gilbert <gilbertariel13@gmail.com>
2022-07-04 11:50:38 +0700

    adding dotenv

--- myApp.js
+++ myApp.js
@@ -1,8 +1,16 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
-console.log("Hello World")
+// console.log("Hello World")
 
+app.get('/json', (req, res) => {
+    if (process.env.VAR_NAME === "allcaps") {
+        res.json = 'Hello World'.toUpperCase();
+    } else {
+        res.json = 'Hello World';
+    }
+  });
 
 
 
